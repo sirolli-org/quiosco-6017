8eb34df3bb898474fa29b03c8782869bbb088cf9
Nicolas A. Serna <serna.nicolas.abel@gmail.com>
2026-04-18 06:53:17 -0300

    up: vistas de ejemplo usando archivos php planos

---
+++ resources/views/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de sesión</title>
+</head>
+<body>
+    <img src="./yaq-logo.svg" alt="Logo de YAQ">
+    <h1>Página de inicio de sesión</h1>
+    <nav>
+        <ul>
+            <li><a href="/">Inicio</a></li>
+            <li>Iniciar sesión</li>
+            <li><a href="/register">Registrarse</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
